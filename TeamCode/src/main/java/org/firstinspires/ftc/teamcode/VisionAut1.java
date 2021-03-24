7c70e98d993a4740ff12f8109b2c6e52fe9c6c25
joshfaber <55210510+joshfaber@users.noreply.github.com>
2021-03-24 15:13:49 -0400

    first attempt at vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAut1.java
@@ -0,0 +1,61 @@
+//TODO: finish step 10 of configuring, adjust rectangles
+
+
+package org.firstinspires.ftc.teamcode;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+
+
+@Autonomous(name="VisionAut1", group="Auton")
+public class VisionAut1 extends LinearOpMode {
+
+    OpenCvCamera phoneCam;
+
+
+    @Override
+    public void runOpMode()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
+        phoneCam.setPipeline(new Pipeline());
+
+        phoneCam.openCameraDevice();
+
+        int ringPos;
+
+    }
+
+
+    class Pipeline extends OpenCvPipeline
+    {
+        boolean viewportPaused = false;
+
+        @Override
+
+        public Mat processFrame(Mat input)
+        {
+
+
+        }
+    }
+
+}
+
+
